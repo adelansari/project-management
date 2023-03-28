3d73ec4f70803682c7b49762d9fd8f3f82da0aee
Adel Ansari <adelansari@github.com>
2023-03-28 17:23:26 +0300

    server connects to mongodb

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+    images: {
+      logoDark: require('./images/logo-dark.png'),
+      appIcon: require('./images/favicon.png')
+    },
+    colors: {
+      secondary: '#001c2e'
+    }
+  }
+  
+  export default assets
