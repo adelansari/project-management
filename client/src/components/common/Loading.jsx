d72b150e732c3b33d7deb2c1cb901d827f0a4e58
Adel Ansari <adelansari@github.com>
2023-03-28 18:10:44 +0300

    loading component

--- client/src/components/common/Loading.jsx
+++ client/src/components/common/Loading.jsx
@@ -1,15 +1,23 @@
 import { Box, CircularProgress } from '@mui/material'
 
-const Loading = props => {
+/**
+ * A component that displays a circular progress indicator in the center of the screen.
+ * 
+ * @param {Object} props - Component props.
+ * @param {boolean} props.fullHeight - Whether the component should have full height (i.e. 100vh).
+ */
+const Loading = (props) => {
   return (
-    <Box sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      height: props.fullHeight ? '100vh' : '100%'
-    }}>
-      <CircularProgress />
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: props.fullHeight ? '100vh' : '100%', // Set the height of the container based on the fullHeight prop.
+      }}
+    >
+      <CircularProgress /> {/* Display a circular progress indicator */}
     </Box>
   )
 }
