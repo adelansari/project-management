9321c6755e47631ab9f705265fbbb52d55b542b2
Adel Ansari <adelansari@github.com>
2023-03-28 17:54:40 +0300

    made a user api for the server

---
+++ server/src/v1/handlers/tokenHandler.js
@@ -0,0 +1,39 @@
+const jsonwebtoken = require('jsonwebtoken')
+const User = require('../models/user')
+
+// Function to decode JWT token from request headers
+const tokenDecode = (req) => {
+  const bearerHeader = req.headers['authorization']
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(' ')[1]
+    try {
+      // Verify JWT token using secret key and return decoded data
+      const tokenDecoded = jsonwebtoken.verify(
+        bearer,
+        process.env.TOKEN_SECRET_KEY
+      )
+      return tokenDecoded
+    } catch {
+      // If token is invalid or expired, return false
+      return false
+    }
+  } else {
+    // If authorization header is not present, return false
+    return false
+  }
+}
+
+// Middleware function to verify JWT token and authenticate user
+exports.verifyToken = async (req, res, next) => {
+  const tokenDecoded = tokenDecode(req)
+  if (tokenDecoded) {
+    // Find user by ID extracted from JWT token
+    const user = await User.findById(tokenDecoded.id)
+    if (!user) return res.status(401).json('Unauthorized')
+    // Add authenticated user object to request object
+    req.user = user
+    next()
+  } else {
+    res.status(401).json('Unauthorized')
+  }
+}
---
+++ server/src/v1/routes/auth.js
@@ -0,0 +1,54 @@
+const router = require('express').Router()
+const userController = require('../controllers/user')
+const { body } = require('express-validator')
+const validation = require('../handlers/validation')
+const tokenHandler = require('../handlers/tokenHandler')
+const User = require('../models/user')
+
+// Route for user registration
+router.post(
+  '/signup',
+  body('username').isLength({ min: 8 }).withMessage(
+    'username must be at least 8 characters'
+  ),
+  body('password').isLength({ min: 8 }).withMessage(
+    'password must be at least 8 characters'
+  ),
+  body('confirmPassword').isLength({ min: 8 }).withMessage(
+    'confirmPassword must be at least 8 characters'
+  ),
+  body('username').custom(value => {
+    // Check if the username is already taken
+    return User.findOne({ username: value }).then(user => {
+      if (user) {
+        return Promise.reject('username already used')
+      }
+    })
+  }),
+  validation.validate, // Validate request
+  userController.register // Register user
+)
+
+// Route for user login
+router.post(
+  '/login',
+  body('username').isLength({ min: 8 }).withMessage(
+    'username must be at least 8 characters'
+  ),
+  body('password').isLength({ min: 8 }).withMessage(
+    'password must be at least 8 characters'
+  ),
+  validation.validate, // Validate request
+  userController.login // Login user
+)
+
+// Route for verifying user token
+router.post(
+  '/verify-token',
+  tokenHandler.verifyToken, // Verify token
+  (req, res) => {
+    res.status(200).json({ user: req.user })
+  }
+)
+
+module.exports = router
